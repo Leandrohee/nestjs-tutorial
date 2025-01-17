3f19d817924371dad70c0f3d83451760929b76e7
leandroTorres <leandrohenrique_@live.com>
2025-01-16 21:03:35 -0300

    chore: add dotenv and a console.log on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config'
 
 async function bootstrap() {
+  console.log(`
+    Server is running in: http://localhost:${process.env.PORT ?? 3000}
+  `)
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
